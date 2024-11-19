2cca5fb9a5aba292f125a0a8c0e649053f80edd2
Rayshaan Fish <Rayshaan94@gmail.com>
2024-11-19 14:13:54 +0200

    fixing the website

--- index.ts
+++ index.ts
@@ -33,3 +33,17 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+const you = {
+  userName: "Bobby",
+  isReturning: true,
+};
+
+function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning) {
+    returningUserDisplay.innerHTML = "back";
+  }
+  userNameDisplay.innerHTML = userName;
+}
+
+populateUser(you.isReturning, you.userName);
